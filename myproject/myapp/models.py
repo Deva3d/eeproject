5962de064757592153fb8a1b41480a159e6e993a
Deva3d <160628532+Deva3d@users.noreply.github.com>
2024-04-14 14:29:45 +0530

    Delete myproject directory

--- myproject/myapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
-
-    class Meta:
-        app_label = 'myapp'  # Specify the app_label here
