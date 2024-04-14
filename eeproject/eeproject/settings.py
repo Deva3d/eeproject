943d4cfd19149e2637b8aa4731ef4ada481f1730
Deva3d <160628532+Deva3d@users.noreply.github.com>
2024-04-14 14:59:46 +0530

    Update settings.py

--- eeproject/eeproject/settings.py
+++ eeproject/eeproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(l5(xt=zqm%#v*zxlw#7zg+6$e_zi!g45lam%94g_4a@o(^-p0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
